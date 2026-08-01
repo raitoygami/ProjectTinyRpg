6ec1701fcce3622a03b19a874d620e24966b34bb
raitoygami <raitoygami@163.com>
2026-08-01 23:05:10 +0800

    角色属性面板

--- Assets/Scripts/Game/System/CameraManager.cs
+++ Assets/Scripts/Game/System/CameraManager.cs
@@ -41,8 +41,8 @@ public class CameraManager : Singleton<CameraManager>
         }
 
         // orthographicSize 僅在 Orthographic 模式才有意義，這裡保留原邏輯
-        _mainCamera.orthographicSize = 7.5f;
-        _followCam.Lens.OrthographicSize = 7.5f;
+        _mainCamera.orthographicSize = 8.4375f;
+        _followCam.Lens.OrthographicSize = 8.4375f;
         _brain = _mainCamera.GetComponent<CinemachineBrain>();
         if (_brain == null)
             _brain = _mainCamera.gameObject.AddComponent<CinemachineBrain>();
